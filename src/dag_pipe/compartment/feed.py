ef9d89b95923476909575157a826553344cc6ca5
Yi Q <yitistica@outlook.com>
2021-01-12 02:56:25 +0800

    modify feed

--- src/dag_pipe/compartment/feed.py
+++ src/dag_pipe/compartment/feed.py
@@ -1,10 +1,13 @@
 from dag_pipe.helpers.elemental.element import Element
 from dag_pipe.helpers.elemental.attributes import Attributes
-from dag_pipe.compartment.validators import check_type
 
 
-_PLACEHOLDER_VAR_NAME = 'place_holder'
-_PARAM_VAR_NAME = 'param'
+_FEED_TYPE_FIELD_NAME = '_type'
+_FEED_TYPE_VALUE_DEFAULT = 'feed'
+_FEED_TYPE_VALUE_PLACEHOLDER = 'placeholder'
+_FEED_TYPE_VALUE_PARAM = 'param'
+
+_PARAM_VALUE_TYPE_VAR_NAME = 'value_type'
 
 
 class AnyValue(object):
@@ -17,19 +20,37 @@ class EmptyValue(object):
 
 class Feed(Element):
     def __init__(self, value, **attributes):
+
+        if _FEED_TYPE_FIELD_NAME not in attributes:
+            attributes = {_FEED_TYPE_FIELD_NAME: _FEED_TYPE_VALUE_DEFAULT, **attributes}
+
         attributes = Attributes(attributes)
         super().__init__(value=value, attributes=attributes)
+
+    @property
+    def type(self):
+        return self.attributes[_FEED_TYPE_FIELD_NAME]
 
 
 class PlaceHolder(Feed):
     def __init__(self, **attributes):
         value = AnyValue
 
-        attributes = {_PLACEHOLDER_VAR_NAME: True, **attributes}
+        attributes = {_FEED_TYPE_FIELD_NAME: _FEED_TYPE_VALUE_PLACEHOLDER, **attributes}
         super().__init__(value=value, **attributes)
 
     def set_value(self, value):
         self.validate(value=value)
-        del self.attributes[_PLACEHOLDER_VAR_NAME]
-        super().__init__(value=value, attributes=self.attributes, validators=self.validator_set.validators)
+        self.set_value(value=value)
 
+
+class Param(Feed):
+    def __init__(self, value, **attributes):
+
+        attributes = {_FEED_TYPE_FIELD_NAME: _FEED_TYPE_VALUE_PARAM, **attributes}
+        attributes = Attributes(attributes)
+
+        super().__init__(value=value, attributes=attributes)
+
+        if _PARAM_VALUE_TYPE_VAR_NAME in self.attributes:
+            pass  # TODO: add validator
